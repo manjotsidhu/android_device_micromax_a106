37760b0965f68479ba81146b98484024d928a07b
Manjot Sidhu <manjot.techie@gmail.com>
2018-11-20 21:08:30 +0530

    a106: camera updates

---
+++ libcam.client/Android.mk
@@ -0,0 +1,20 @@
+# wrapper library for libcam.halsensor
+#
+LOCAL_PATH := $(call my-dir)
+ifeq ($(USE_MTK_CAMERA_WRAPPER),true)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    StreamImgBuf.cpp \
+
+LOCAL_C_INCLUDES := \
+    $(TOP)/system/media/camera/include \
+$(TOP)/frameworks/native/libs/nativebase/include
+
+LOCAL_SHARED_LIBRARIES := liblog libutils libcutils libdl \
+			  libcam_utils
+
+LOCAL_MODULE := libcam.client
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
+endif
